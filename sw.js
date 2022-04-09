b4259a0f40da7feea0c48b7ea8eaf0813dabbcd8
fjc-orange-creation <245865571@qq.com>
2022-04-09 23:36:04 +0800

    Site updated: 2022-04-09 23:36:04

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/16207/','/posts/16107/','/posts/38499/','/posts/55821/','/posts/54465/','/categories/index.html','/about/index.html','/icons/index.html','/gallery/index.html','/library/index.html','/extension/content.json','/records/index.html','/letter/index.html','/resume/index.html','/records/content.json','/tags/index.html','/extension/letter/index.css','/extension/resume/index.css','/extension/letter/index.js','/extension/test/index.html','/extension/resume/index.js','/extension/resume/chinese/index.html','/extension/resume/chinese/data.json','/extension/letter/english/data.json','/extension/letter/english/index.html','/extension/letter/chinese/data.json','/extension/letter/chinese/index.html','/extension/resume/english/data.json','/extension/resume/english/index.html',
+'/','/posts/16207/','/posts/16107/','/posts/38499/','/posts/55821/','/posts/54465/','/gallery/index.html','/categories/index.html','/icons/index.html','/about/index.html','/extension/content.json','/library/index.html','/letter/index.html','/resume/index.html','/tags/index.html','/records/content.json','/records/index.html','/extension/letter/index.css','/extension/resume/index.js','/extension/letter/index.js','/extension/letter/chinese/data.json','/extension/test/index.html','/extension/letter/chinese/index.html','/extension/letter/english/data.json','/extension/resume/index.css','/extension/letter/english/index.html','/extension/resume/english/data.json','/extension/resume/english/index.html','/extension/resume/chinese/index.html','/extension/resume/chinese/data.json',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
